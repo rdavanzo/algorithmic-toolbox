5637268559ed0fe7458d418d0820c6121612c4a6
Ryan Davanzo <ryan@esha.com>
2017-11-05 23:43:57 -0800

    Add week 2 assignments

---
+++ src/GreatestCommonDivisor/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace GreatestCommonDivisor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Constraint: 1 ≤ 𝑎, 𝑏 ≤ 2e9
+            var ary = Console.ReadLine().Split()
+                .Take(2)
+                .Select(x => Convert.ToUInt64(x))
+                .ToArray();
+            var a = ary[0];
+            var b = ary[1];
+            var result = gcd_fast(a, b);
+
+            Console.WriteLine(result);
+        }
+
+        private static UInt64 gcd_naive(UInt64 a, UInt64 b) 
+        {
+            var current_gcd = 1UL;
+            for(var d = 2UL; d <= a && d <= b; ++d) 
+            {
+                if (a % d == 0 && b % d == 0) 
+                {
+                    if (d > current_gcd) 
+                    {
+                        current_gcd = d;
+                    }
+                }
+            }
+
+            return current_gcd;
+        }
+
+        private static UInt64 gcd_fast(UInt64 a, UInt64 b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+
+            return gcd_fast(b, a % b);
+        } 
+    }
+}
