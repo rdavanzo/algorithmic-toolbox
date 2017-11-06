4bc217460f71cc015a36a3124e61179458ff0045
Ryan Davanzo <ryan@esha.com>
2017-11-05 23:45:28 -0800

    Add week 2 assignments

---
+++ src/LargeFibonacciLastDigit/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace LargeFibonacciLastDigit
+{
+    class Program
+    {
+        private static UInt64 hugeFibonacciNaive(UInt64 n)
+        {
+            if (n <= 1ul)
+            {
+                return n;
+            }
+
+            var previous = 0ul;
+            var current = 1ul;
+            for (var i = 0ul; i < n - 1ul; i++)
+            {
+                var tmp = previous;
+                previous = current;
+                current = tmp + current;
+                //Console.WriteLine($"{current} = {tmp} + {current}");
+            }
+            var result = current % 10ul;
+            
+            return result;
+        }
+
+        private static Int32 hugeFibonacciFast(Int32 n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            var ary = new Int32[n + 1];
+            ary[0] = 0;
+            ary[1] = 1;
+            for (var i = 2; i <= n; i++)
+            {
+                ary[i] = (ary[i - 1] + ary[i - 2]) % 10;
+            }
+
+            return ary[n];
+        }
+
+        private static void runAssertions()
+        {
+            var result = hugeFibonacciNaive(239ul);
+            if (result != 1)
+            {
+                Console.WriteLine($"Wrong answer for n = 239: {result}.");
+            }
+        }
+
+        private static void stressTest()
+        {
+            for (var n = 1; n <= 300; n++)
+            {
+                var slow = hugeFibonacciNaive((UInt64)n);
+                var fast = hugeFibonacciFast(n);
+                if (slow != (UInt64)fast)
+                {
+                    Console.WriteLine($"Wrong : {slow} {fast}");
+                }
+                else
+                {
+                    Console.WriteLine("Ok");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //runAssertions();
+            //Console.WriteLine("Assertions complete.");
+
+            // stressTest();
+            // Console.WriteLine("Stress test complete.");
+
+            var n = Convert.ToInt32(Console.ReadLine());
+            var result = hugeFibonacciFast(n);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ src/SmallFibonacciNumber/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace SmallFibonacciNumber
+{
+    class Program
+    {
+        private static Int32 fibonacciRecursive(Int32 n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+        }
+
+        private static Int32 fibonacciIterative(Int32 n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            var ary = new Int32[n + 1];
+            ary[0] = 0;
+            ary[1] = 1;
+            for (var i = 2; i <= n; i++)
+            {
+                ary[i] = ary[i - 1] + ary[i - 2];
+            }
+
+            return ary[n];
+        }
+
+        private static void runAssertions()
+        {
+            var a = fibonacciIterative(1);
+            if (a != 1)
+            {
+                Console.WriteLine($"Wrong answer for n = 1: {a}.");
+            }
+            
+            var b = fibonacciIterative(2);
+            if (b != 1)
+            {
+                Console.WriteLine($"Wrong answer for n = 2: {b}.");
+            } 
+
+            var c = fibonacciIterative(10);
+            if (c != 55)
+            {
+                Console.WriteLine($"Wrong answer for n = 10: {c}.");
+            }
+        }
+
+        private static void stressTest()
+        {
+            for (var n = 1; n <= 20; n++)
+            {
+                var slow = fibonacciRecursive(n);
+                var fast = fibonacciIterative(n);
+                if (slow != fast)
+                {
+                    Console.WriteLine($"Wrong : {slow} {fast}");
+                }
+                else
+                {
+                    Console.WriteLine("Ok");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // runAssertions();
+            // Console.WriteLine("Assertions complete.");
+
+            // stressTest();
+            // Console.WriteLine("Stress test complete.");
+
+            var n = Convert.ToInt32(Console.ReadLine());
+            var smallFibNumber = fibonacciIterative(n);
+            Console.WriteLine(smallFibNumber);
+        }
+    }
+}
