3ea259369a173d34ce8c5818c423b73c75c26ba6
Ryan Davanzo <ryan@esha.com>
2017-11-09 09:31:11 -0800

    Adding week 3 changing money program

---
+++ src/ChangingMoney/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ChangingMoney
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 <= m <= 10e3
+            var m = Convert.ToInt32(Console.ReadLine());
+            var coinCount = makeChange(m);
+            Console.WriteLine(coinCount);
+        }
+
+        private static Int32 makeChange(Int32 m)
+        {
+            var dimeCount = m / 10;
+            m = m % 10;
+            if (m == 0)
+            {
+                return dimeCount;
+            }
+
+            var nickelCount = m / 5;
+            m = m % 5;
+            if (m == 0)
+            {
+                return dimeCount + nickelCount;
+            }
+
+            var pennyCount = m / 1;
+            
+            return dimeCount + nickelCount + pennyCount;
+        }
+    }
+}
